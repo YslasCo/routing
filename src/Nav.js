b66780ffd899dcd24277327dacc440ede0ce6ec9
Jared Yslas <jryslas@gmail.com>
2019-09-09 21:10:27 -0700

    functional pages as components

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const navStyle = {
+    color: "white"
+  };
+
   return (
     <nav>
       <h3>Logo</h3>
       <ul className="nav-links">
-        <li>About</li>
-        <li>Shop</li>
+        <Link style={navStyle} to="/About">
+          <li>About</li>
+        </Link>
+        <Link style={navStyle} to="/Shop">
+          <li>Shop</li>
+        </Link>
       </ul>
     </nav>
   );
